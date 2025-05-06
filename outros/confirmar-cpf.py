03a34f5832ea7539feed4396a90b90ac2589b639
Caio Assis <caio.ab.assis@gmail.com>
2025-05-06 20:38:02 -0300

    Altera comentarios

--- outros/confirmar-cpf.py
+++ outros/confirmar-cpf.py
@@ -4,6 +4,8 @@ m = 9
 somaAux = 0
 vrfAux = 0
 
+#apenas numeros, cpf tem 9 numeros e 2 digitos verificadores
+#primeiro dig ver = multiplicar cada casa do cpf (9 nums) como se fossem unidades, para cada casa, soma +1 no multiplicador, começando com 2
 if len(cpf) == 11:
     for n in range(9):
         num.append(int(cpf[n]))
@@ -22,6 +24,7 @@ if len(cpf) == 11:
     somaAux=0
     m=10
 
+#segundo dig ver = multiplicar cada casa do cpf (9 nums + primeiro dig ver, sendo considerado esse o primeiro digito) como se fossem unidades, para cada casa, soma +1 no multiplicador, começando com 2. 
     for n in range(10):
         m = m-1
         somaAux = somaAux + (num[m] * (n+2))
